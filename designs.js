414e398580cee67222e31437090a8ee469c58e04
Santhosh Kumar Sellavel <skrece22@gmail.com>
2018-01-22 00:18:28 +0530

    first commit

---
+++ designs.js
@@ -0,0 +1,121 @@
+$(window).on('load', function () {
+  $('button').attr('disabled', 'disabled');
+});
+
+//Variables
+const form = $("form");
+const pixelCanvas = $("#pixel_canvas");
+const colorPicker = $("#colorPicker");
+
+let gridHeight = document.getElementById("input_height").value;
+let gridWidth = document.getElementById("input_width").value;
+let color = document.getElementById("colorPicker").value;
+
+let mouseDown = false;
+let borderRemoved = false;
+let eraserOn = false;
+
+
+//Setting default Color for colorPicker
+colorPicker.val("#aabbcc");
+
+//Colorpicker Listener
+colorPicker.change(function (evt) {
+  if(!eraserOn){
+    color = evt.target.value;
+  } else{
+    resetEraser();
+  }
+});
+
+// Mouse Listeners for performing drawing 
+pixelCanvas.on("mousedown", "td", function (evt) {
+  $(evt.target).css("background-color", color);
+  mouseDown = true;
+});
+
+$('html').on("mouseup","td", function (evt) {
+  mouseDown = false;
+});
+
+pixelCanvas.on("mouseover", "td", function (evt) {
+  if (mouseDown) {
+    $(evt.target).css("background-color", color);
+  }
+});
+
+// Adding or Removing border
+$('.border').click(function () {
+  var $this = $(this);
+  borderRemoved = !borderRemoved;
+  removeBorder();
+  if(borderRemoved){
+    $this.text("Add Border")
+  }
+  else {
+    $this.text("Remove Border")
+  }
+});
+
+// A function to remove table, td, tr border
+function removeBorder() {
+  $('td').toggleClass('removedBorder');
+  $('table').toggleClass('removedBorder');
+  $('tr').toggleClass('removedBorder');
+}
+
+// Enable or Disable Eraser
+$('.eraser').click(function () {
+  var $this = $(this);
+  eraserOn = !eraserOn;
+  if (eraserOn) {
+    color = "#FFFFFF";
+    $this.text("Click to Draw!")
+  } else {
+    color = colorPicker.val();
+    $this.text("Click to Erase!")
+  }
+});
+
+//Form
+form.submit(function (evt) {
+  evt.preventDefault();
+  resetBorder();
+  resetEraser();
+  gridHeight = evt.target.height.value;
+  gridWidth = evt.target.width.value;
+  makeGrid(gridHeight, gridWidth, color);
+});
+
+//make grid function
+function makeGrid(height, width) {
+  pixelCanvas.children().remove();
+  let td = "";
+  for (let j = 0; j < width; j++) {
+    td += "<td></td>";
+  }
+  for (let i = 0; i < height; i++) {
+    pixelCanvas.append(`<tr>${td}</tr>`);
+  }
+}
+
+//reset border
+
+function resetBorder() {
+  $('button').removeAttr('disabled');
+  if ($('.border').text() === 'Add Border') {
+    borderRemoved = false;
+    $('.border').text("Remove Border");
+    removeBorder();
+  }
+}
+
+//resetEraser
+
+function resetEraser() {
+  if ($('.eraser').text() === 'Click to Draw!') {
+    eraserOn = false;
+    color = colorPicker.val();
+    $('.eraser').text("Click to Erase!")
+  }
+}
